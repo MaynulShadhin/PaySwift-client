a53a00eb5b05290f8dfab3b9a33a48112bdc95d7
Md. Maynul Islam <islammaynul82@gmail.com>
2024-07-16 16:01:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <button className="btn btn-outline">Button</button>
+    </>
+  )
+}
+
+export default App
